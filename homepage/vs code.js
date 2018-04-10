11076072a13e713046d9e0eacf427ae753d4e044
zwlafk <wlongtony@gmail.com>
2018-04-10 21:23:40 +0800

    vscode config

---
+++ homepage/vs code.js
@@ -0,0 +1,9 @@
+{
+    "workbench.colorTheme": "Monokai",
+    "window.zoomLevel": 0,
+    "editor.fontSize": 16,
+    "python.linting.enabledWithoutWorkspace": false,
+    "sublimeTextKeymap.promptV3Features": true,
+    "explorer.confirmDelete": false,
+    "editor.fontFamily": "Menlo, Monaco, 'Courier New', monospace"
+}
